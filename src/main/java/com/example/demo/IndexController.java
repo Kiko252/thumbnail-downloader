2ce389e86c6aa0af4189a75ca094c8eb7cfbfe71
Kiko252 <82182276+Kiko252@users.noreply.github.com>
2023-01-16 10:32:34 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        return "Thumbnail";
+    }
+
+    String ytImages;
+    String videoID;
+
+    @PostMapping("/getUrl")
+    public String getUrl(@ModelAttribute Url url) {
+
+        String videoUrl = url.getUrl();
+
+        if (videoUrl.contains("v=")) {
+            int index = videoUrl.indexOf("v=");
+            videoID = videoUrl.substring(index + 2, index + 13);
+
+            ytImages = "https://img.youtube.com/vi/vdID/maxresdefault.jpg";
+            ytImages = ytImages.replace("vdID", videoID);
+        }
+
+
+        return "Download";
+
+    }
+
+
+    @GetMapping("/show")
+    public RedirectView show() {
+        RedirectView redirectView = new RedirectView(ytImages, true);
+        redirectView.setExposeModelAttributes(false);
+        redirectView.setHttp10Compatible(false);
+        return redirectView;
+    }
+
+
+    @GetMapping("/download")
+    public String downloadImage() throws InterruptedException {
+        if (ytImages != null) {
+            String home = System.getProperty("user.home");
+            String imagePath = home + "/Downloads/thumbnail_" + videoID + ".jpg";
+
+            BufferedImage image;
+            try {
+
+                URL urlImg = new URL(ytImages);
+                image = ImageIO.read(urlImg);
+
+                ImageIO.write(image, "jpg", new File(imagePath));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Thread.sleep(1000);
+
+        return "Download";
+    }
+}
