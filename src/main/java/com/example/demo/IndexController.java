9210d79de4632a58478ceaef2e76b4f8ae49d972
Hristijan Popovski <hristijan.popovski16@gmail.com>
2023-05-11 01:14:18 +0300

    Cleane code in controller

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -8,53 +8,45 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private Url oUrl;
+    private String ytImages;
+    private String videoID;
+
     @GetMapping("/")
     public String index(Model model) {
-
         return "Thumbnail";
     }
 
-    @Autowired
-    Url oUrl;
-    String ytImages;
-    String videoID;
-
     @PostMapping("/getUrl")
     public String getUrl(@ModelAttribute Url url, Model model) {
-
-
         String videoUrl = url.getUrl();
 
         if (videoUrl.contains("v=")) {
             int index = videoUrl.indexOf("v=");
             videoID = videoUrl.substring(index + 2, index + 13);
-
-            ytImages = "https://img.youtube.com/vi/vdID/maxresdefault.jpg";
-            ytImages = ytImages.replace("vdID", videoID);
+            ytImages = "https://img.youtube.com/vi/" + videoID + "/maxresdefault.jpg";
         }
         oUrl.setUrl(ytImages);
-        model.addAttribute("oUrl",oUrl);
+        model.addAttribute("oUrl", oUrl);
 
         return "Download";
-
     }
-
 
     @GetMapping("/show")
     public RedirectView show() {
-        System.out.println();
         RedirectView redirectView = new RedirectView(oUrl.getUrl(), true);
         redirectView.setExposeModelAttributes(false);
         redirectView.setHttp10Compatible(false);
         return redirectView;
     }
-
 
     @GetMapping("/download")
     public String downloadImage(Model model) throws InterruptedException {
@@ -64,19 +56,16 @@ public class IndexController {
 
             BufferedImage image;
             try {
-
                 URL urlImg = new URL(ytImages);
                 image = ImageIO.read(urlImg);
-
                 ImageIO.write(image, "jpg", new File(imagePath));
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         Thread.sleep(1000);
 
-        model.addAttribute("oUrl",oUrl);
+        model.addAttribute("oUrl", oUrl);
 
         return "Download";
     }
